1ce84645775de61514fe4850d9bf4a948d9cd630
lanxeyu <lanxeyu@gmail.com>
2023-10-21 17:42:04 +0100

    Port working

--- app.js
+++ app.js
@@ -8,4 +8,10 @@ app.use(express.json())
 app.use(cors())
 app.use(logger('dev'))
 
+app.get("/", (req, res) => {
+    res.json({
+        title: "Pluto's Pizza Sales",
+    })
+})
+
 module.exports = app
