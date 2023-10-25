83bf91bc9f33005b9171db1f2c2d1371c19e2328
lanxeyu <lanxeyu@gmail.com>
2023-10-25 20:25:51 +0100

    Remove unused post/subscribe route

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const logger = require('morgan');
 const webpush = require('web-push');
 const bodyParser = require('body-parser');
 const path = require('path');
-const { sendPushNotification } = require('./services/notification.js');
 
 const orderRoutes = require('./routes/order');
 
@@ -20,21 +19,6 @@ const publicVapidKey = 'BIgp7JGC-EZKn2Fcvt7iBFQLlDj-iVzWU3I8ZlzrwhSuzXRHa23KsVmy
 const privateVapidKey = 'nFRrhg3G_giv5lh19z3Gq1nNoqF2GuirHAv4lYomuDM';
 
 webpush.setVapidDetails('mailto:test@test.com', publicVapidKey, privateVapidKey);
-
-app.post('/subscribe', async (req, res) => {
-    try {
-      const subscription = req.body;
-      const payload = { title: 'Push Test' };
-  
-      await sendPushNotification(subscription, payload);
-  
-      res.status(201).json({ message: 'Push notification sent' });
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Error sending push notification' });
-    }
-});
-  
 
 app.use(express.json())
 app.use(logger('dev'))
